52017885ab3c914575bf149c3ac5d65d5c4edb0d
Mingi Kang <mkang2@bowdoin.edu>
2025-12-13 15:10:11 -0500

    goon sort

--- treeNash.py
+++ treeNash.py
@@ -35,7 +35,7 @@ def parse(file):
 
     for i in range(1, number_nodes + 1):
         random_state = 1 if random() < 0.5 else 0
-        graph.add_node(i, state=random_state, matrix={}, num_parents=0)
+        graph.add_node(i, state=random_state, matrix={}, num_parents=0, parents = [])
     
     for i, line in enumerate(lines[2:2 + number_nodes]):
         row = list(map(int, line.strip().split()))
@@ -43,43 +43,44 @@ def parse(file):
     print("adjacency", adjacency)
     initialize_adjacency_matrix(adjacency)
     print("edges", graph.edges())
-    print("nodes", graph.nodes(data=True))
-
-    # Initialize payoff matrix for each node
-    # parent_list = [a for a, b in graph.edges()]
-    # print("parent_list", parent_list)
-    # child_list = [b for a, b in graph.edges()]
-    # print("child_list", child_list)
-
 
     for node in graph.nodes():
         for a, b in graph.edges():
             if b == node:
                 graph.nodes[node]['num_parents'] += 1
+                graph.nodes[node]['parents'].append(a)
                 
             
         
     print("nodes with num_parents", graph.nodes(data=True))
 
-
+"""
+nodes with num_parents 
+[
+    (1, {'state': 1, 'matrix': {}, 'num_parents': 0, 'parents': []}), 
+    (2, {'state': 1, 'matrix': {}, 'num_parents': 1, 'parents': [1]}), 
+    (3, {'state': 0, 'matrix': {}, 'num_parents': 1, 'parents': [1]}), 
+    (4, {'state': 0, 'matrix': {}, 'num_parents': 1, 'parents': [3]})]
+None
+"""
     
-    print()
-    for node in graph.nodes():
-        matrix = {}
-        print("node", node)
-        print("neighbors", list(graph.neighbors(node)))
+    # print()
+    # for node in graph.nodes():
+    #     matrix = {}
+    #     print("node", node)
+    #     print("neighbors", list(graph.neighbors(node)))
 
-        ## 
+    #     ## 
         
-        for neighbor in graph.neighbors(node):
-            print()
-            matrix[neighbor] = {
-                (0, 0): 1,
-                (0, 1): 0,
-                (1, 0): 0,
-                (1, 1): 1
-            }
-        graph.nodes[node]['matrix'] = matrix
+    #     for neighbor in graph.neighbors(node):
+    #         print()
+    #         matrix[neighbor] = {
+    #             (0, 0): 1,
+    #             (0, 1): 0,
+    #             (1, 0): 0,
+    #             (1, 1): 1
+    #         }
+    #     graph.nodes[node]['matrix'] = matrix
 
 """
 edges [(1, 2), (1, 3), (3, 4)]
